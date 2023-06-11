7c2d3d8cba74b4d9feb4eb0530c9cd338092b593
HYT111222 <HeYiTing2023@outlook.com>
2023-06-11 21:37:35 +0800

    部分修改

--- src/main/java/com/DepositApplication.java
+++ src/main/java/com/DepositApplication.java
@@ -5,12 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@SpringBootApplication(scanBasePackages = "com" )
-//@Configuration
-//@ComponentScan({"com.service"})
 @MapperScan({"com.mapper"})
 public class DepositApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DepositApplication.class, args);
     }
