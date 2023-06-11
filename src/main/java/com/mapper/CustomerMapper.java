e86c8ec3f1281ee3a37492ab8d002b2951176dd2
HYT111222 <HeYiTing2023@outlook.com>
2023-06-11 14:35:32 +0800

    初步构建

--- src/main/java/com/mapper/CustomerMapper.java
+++
@@ -1,9 +0,0 @@
-package com.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.entity.Customer;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface CustomerMapper extends BaseMapper<Customer> {
-}
